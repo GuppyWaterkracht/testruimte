56317eb720cacee195f57229d83f4db7ff2a7322
GuppyWaterkracht <eliora@cammera.at>
2024-06-16 15:38:19 +0200

    het is nu de ontvanger van de gamepad

--- main.ts
+++ main.ts
@@ -1,22 +1,37 @@
 input.onButtonPressed(Button.A, function () {
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    graden += 1
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString("gestolen")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "VL") {
+        maqueen.servoRun(maqueen.Servos.S1, 71)
+    } else if (receivedString == "VH") {
+        maqueen.servoRun(maqueen.Servos.S1, 0)
+    } else if (receivedString == "stop") {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    graden += -1
 })
-maqueen.servoRun(maqueen.Servos.S1, 0)
-basic.forever(function () {
-    music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
-    music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+radio.onReceivedValue(function (name, value) {
+    if (name == "Y" && value > 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, value / 2)
+    }
+    if (name == "Y" && value < 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, value / -2)
+    }
+    if (name == "X" && value > 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, value / 2)
+    }
+    if (name == "X" && value > 0) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, value / -2)
+    }
 })
+let graden = 0
+let value = 0
 basic.forever(function () {
-    basic.pause(1000)
-    maqueen.servoRun(maqueen.Servos.S1, 0)
-    basic.pause(1000)
-    maqueen.servoRun(maqueen.Servos.S1, 180)
-})
-basic.forever(function () {
-    music.setVolume(pins.analogReadPin(AnalogPin.P1))
+    basic.showNumber(graden)
 })
